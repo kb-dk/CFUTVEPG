0054aef3065a99bd6111f69f370f26b6771da41b
Anders Selling Jensen <asj@statsbiblioteket.dk>
2012-09-24 14:33:51 +0200

    added 2 missing files

---
+++ src/main/java/testing/persistence/CompositeProgramDAO.java
@@ -0,0 +1,38 @@
+package testing.persistence;
+
+import dk.statsbiblioteket.digitaltv.access.model.CompositeProgram;
+import dk.statsbiblioteket.digitaltv.access.model.RitzauProgram;
+import dk.statsbiblioteket.mediaplatform.ingest.model.persistence.GenericHibernateDAO;
+import dk.statsbiblioteket.mediaplatform.ingest.model.persistence.HibernateUtilIF;
+import org.hibernate.Criteria;
+import org.hibernate.criterion.Criterion;
+import org.hibernate.criterion.Order;
+import org.hibernate.criterion.Restrictions;
+
+import java.util.List;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: asj
+ * Date: 21-09-12
+ * Time: 11:03
+ * To change this template use File | Settings | File Templates.
+ */
+public class CompositeProgramDAO extends GenericHibernateDAO<RitzauProgram, Long> {
+    public CompositeProgramDAO(HibernateUtilIF util){
+        super(RitzauProgram.class, util);
+    }
+
+    public boolean hasTvMeter(RitzauProgram ritzauProgram){
+        Criteria criteria = getSession().createCriteria(CompositeProgram.class);
+        Criterion criterion = Restrictions.eq("ritzauProgram", ritzauProgram);
+        criteria.add(criterion);
+        CompositeProgram compositeProgram = (CompositeProgram) criteria.uniqueResult();
+        if(compositeProgram != null){
+            if(compositeProgram.getTvmeterProgram() != null){
+                return true;
+            }
+        }
+        return false;
+    }
+}
